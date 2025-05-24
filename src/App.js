b9bfd2a36f80c328bba69710ab93c56e6a0d9246
EswaramoorthyS <smeswaramoorthy@gmail.com>
2025-05-24 19:25:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        
-       
+       <Header />
+        <img src="https://www.metaltrader.com/logo.png" className="App-logo" alt="Metal Trader Logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        
       <Home />
 
       </header>
